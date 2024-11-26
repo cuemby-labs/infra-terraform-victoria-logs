8e7d817bd89a884543edbe694cf3a2f3990d7e3d
Lewisjmp <lewis.munoz@cuemby.com>
2024-11-26 14:12:16 -0500

    Victoria Logs config

--- main.tf
+++ main.tf
@@ -1,9 +1,25 @@
-locals {
-  context = var.context
+#
+# Victoria Metrics Resources
+#
+resource "kubernetes_namespace" "victoria_logs" {
+  metadata {
+    name = var.namespace_name
+  }
 }
 
-module "submodule" {
-  source = "./modules/submodule"
+resource "helm_release" "victoria_logs" {
+  name       = var.helm_release_name
+  namespace  = var.namespace_name
+  repository = "https://victoriametrics.github.io/helm-charts/"
+  chart      = "victoria-logs-single"
+  version    = var.helm_chart_version
+  values     = [file("${path.module}/values.yaml")]
+}
 
-  message = "Hello, submodule"
+#
+# Walrus Information
+#
+
+locals {
+  context = var.context
 }
