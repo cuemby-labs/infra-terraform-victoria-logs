41486eae1fceca49eb20ef3e004c7a5e3523c679
Lewisjmp <lewis.munoz@cuemby.com>
2024-11-28 10:32:02 -0500

    Victoria Logs refactor main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,20 @@
 #
 # Victoria Metrics Resources
 #
-resource "kubernetes_namespace" "victoria_logs" {
+
+# Validar si el namespace ya existe
+data "kubernetes_namespace" "victoria_system" {
   metadata {
     name = var.namespace_name
   }
+}
+
+# Crear el namespace solo si no existe
+resource "kubernetes_namespace" "victoria_system" {
+  metadata {
+    name = var.namespace_name
+  }
+  count = length(data.kubernetes_namespace.victoria_system.id) == 0 ? 1 : 0
 }
 
 resource "helm_release" "victoria_logs" {
@@ -14,6 +24,8 @@ resource "helm_release" "victoria_logs" {
   chart      = "victoria-logs-single"
   version    = var.helm_chart_version
   values     = [file("${path.module}/values.yaml")]
+
+    depends_on = [kubernetes_namespace.victoria_system]
 }
 
 #
